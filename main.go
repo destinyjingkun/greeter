1a881757b060697038c286e97a0b314f6217b317
陈境坤 <evan@chenjingkundeMacBook-Pro.local>
2018-11-08 15:59:04 +0800

    init

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/micro/go-log"
+	"github.com/micro/go-micro"
+	"greeter/handler"
+	"greeter/subscriber"
+
+	example "greeter/proto/example"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.srv.greeter"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	example.RegisterExampleHandler(service.Server(), new(handler.Example))
+
+	// Register Struct as Subscriber
+	micro.RegisterSubscriber("go.micro.srv.greeter", service.Server(), new(subscriber.Example))
+
+	// Register Function as Subscriber
+	micro.RegisterSubscriber("go.micro.srv.greeter", service.Server(), subscriber.Handler)
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
